d582ee38e23d9e662e47a67fcd8b68a2415975b9
Maninder <maninder1516@gmail.com>
2025-08-03 00:52:34 +0530

    Task component is added

--- src/App1.vue
+++ src/App1.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { reactive } from 'vue';
-import Task from './components/Task.vue';
-
 
 // ref for Primitives : number, strings, boolean etc
 const appName = 'Task Manager with Pinia';
@@ -96,9 +94,19 @@ function addTask(){
       </div>
     </div>
      
-    <!-- Tasks -->
-    <div class="tasks">
-      <Task v-for="(task, index) in tasks" :key="index" :task="task"></Task>      
+    <div v-for="(task, index) in tasks" :key="index" class="task">
+        <h3>
+          {{ task.name }}
+        </h3>
+        <p>
+          {{ task.description }}
+        </p>
+        <div class="task-check">
+          <input type="checkbox" :checked="task.completed " />
+          <label>
+            Done
+          </label>
+        </div>
     </div>
 
     <div class="add-task">
@@ -177,6 +185,84 @@ function addTask(){
 
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, 1fr);
+  }
+}
+
+.task {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--white-color);
+  color: var(--black-color);
+  padding: 20px;
+  border-radius: 12px;
+  position: relative;
+  margin:5px;
+
+
+  h3 {
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 21px;
+    letter-spacing: 0em;
+    text-align: left;
+  }
+
+  p {
+    margin-top: 24px;
+    margin-bottom: 12px;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 16px;
+    letter-spacing: 0em;
+    text-align: left;
+  }
+
+
+  .task-check {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+
+    label {
+      font-size: 13px;
+      font-weight: 400;
+      line-height: 16px;
+      letter-spacing: 0em;
+      text-align: left;
+      margin-left: 5px;
+      cursor: pointer;
+    }
+
+    input {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 18px;
+      height: 18px;
+      border-radius: 100%;
+      border: 0.77px solid #AEAEB2;
+      appearance: none;
+      cursor: pointer;
+
+
+      &:checked {
+        background-color: #0A7AFF;
+        border-color: #0A7AFF;
+
+        &::before {
+          content: '';
+          display: block;
+          width: 4.5px;
+          height: 9px;
+          border: solid white;
+          border-width: 0 2px 2px 0;
+          transform: rotate(45deg);
+        }
+      }
+    }
   }
 }
 
